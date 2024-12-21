1dd1ee68db178218290985cd098f915ee2df6b26
Ligo George <ligo.george@tractive.com>
2024-12-21 21:56:28 +0100

    add: auto-session plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -16,6 +16,19 @@ local plugins = {
     },
 
     {
+        'rmagatti/auto-session',
+        lazy = false,
+
+        ---enables autocomplete for opts
+        ---@module "auto-session"
+        ---@type AutoSession.Config
+        opts = {
+          suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
+          -- log_level = 'debug',
+        }
+    },
+
+    {
         "ThePrimeagen/vim-be-good",
         lazy = false,
         cmd = { "VimBeGood" },
